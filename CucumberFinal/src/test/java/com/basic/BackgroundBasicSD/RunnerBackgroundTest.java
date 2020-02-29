dc27a78f5b2fa33d89207c9335f28c4d45b0101f
Rashmi <Rashmi@DESKTOP-PQQMPAH>
2020-02-29 18:56:59 +0530

    report added commit

--- CucumberFinal/src/test/java/com/basic/BackgroundBasicSD/RunnerBackgroundTest.java
+++ CucumberFinal/src/test/java/com/basic/BackgroundBasicSD/RunnerBackgroundTest.java
@@ -7,7 +7,6 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		
 	features= {"src/test/resources/com/basic/BackgroundBasicFF/"},
 	glue= {"com/basic/BackgroundBasicSD/"},
 	monochrome = true,
